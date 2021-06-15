5b1969e38317a468d2a891d1547d7b536ef7d138
Yichuan Feng <yichuaf@amazon.com>
2021-06-15 20:51:34 +0000

    Bind subscription based on order change event

--- src/PendingOrders.js
+++ src/PendingOrders.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 import Amplify, {API, graphqlOperation} from 'aws-amplify';
 import * as qu from './graphql/queries';
 import * as mu from './graphql/mutations';
-
-
-
-
-
-
+import * as su from './graphql/subscriptions';
 
 class PendingOrders extends Component {
 
@@ -21,6 +16,28 @@ async fetchOrders() {
     }
 }
 
+subscribeOrderEvent() {
+     API.graphql(graphqlOperation(su.onCreateOrder)).subscribe({
+         next: ({ provider, value }) => console.log({ provider, value }),
+         error: error => console.warn(error)
+     });
+
+     API.graphql(graphqlOperation(su.onUpdateOrder)).subscribe({
+              next: ({ provider, value }) => console.log({ provider, value }),
+              error: error => console.warn(error)
+          });
+
+      API.graphql(graphqlOperation(su.onCreateMenu)).subscribe({
+                    next: ({ provider, value }) => console.log({ provider, value }),
+                    error: error => console.warn(error)
+                });
+}
+
+componentDidMount() {
+    console.log("Orders component loaded");
+    this.subscribeOrderEvent();
+    console.log("Subscribed on the order change event");
+}
 
  render() {
   return (
